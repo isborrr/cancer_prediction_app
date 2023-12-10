6dcc50b0e7a0ba419586877825ef5152fb21d795
isborrr <63576749+isborrr@users.noreply.github.com>
2023-12-09 21:36:39 -0500

    Create RandomForest.java
    Added Random Forest

---
+++ CSCI370_Software_Engineering_Project/src/RandomForest.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomForest {
+
+	private final List<DecisionTree> trees;
+
+	public RandomForest(int numTrees) {
+		this.trees = new ArrayList<>();
+		for (int i = 0; i < numTrees; i++) {
+			DecisionTree tree = new DecisionTree();
+			trees.add(tree);
+		}
+	}
+
+	public void train(double[][] data, String[] features, int targetIndex) {
+		Random random = new Random();
+
+		for (DecisionTree tree : trees) {
+			// Create a bootstrapped sample
+			double[][] bootstrapSample = createBootstrapSample(data, random);
+
+			// Train each tree on a different subset of the data
+			tree.train(bootstrapSample, features, targetIndex);
+		}
+	}
+
+	public double predict(double[] instance) {
+		double sum = 0.0;
+
+		for (DecisionTree tree : trees) {
+			sum += tree.predict(tree.getRoot(), instance);
+		}
+
+		// Return the average prediction from all trees
+		return sum / trees.size();
+	}
+
+	private double[][] createBootstrapSample(double[][] data, Random random) {
+		int numInstances = data.length;
+		double[][] bootstrapSample = new double[numInstances][];
+
+		for (int i = 0; i < numInstances; i++) {
+			int randomIndex = random.nextInt(numInstances);
+			bootstrapSample[i] = data[randomIndex].clone();
+		}
+
+		return bootstrapSample;
+	}
+
+	public static void main(String[] args) {
+		// Example usage
+		double[][] data = {
+				// Add your data here
+		};
+
+		String[] features = { "feature1", "feature2", /* Add more features as needed */ };
+		int targetIndex = 2/* Index of the target variable in features */;
+
+		RandomForest randomForest = new RandomForest(5);
+		randomForest.train(data, features, targetIndex);
+
+		// Example instance for prediction
+		double[] instance = { /* Provide values for your features here */ };
+		double prediction = randomForest.predict(instance);
+		System.out.println("Random Forest Prediction for the instance: " + prediction);
+	}
+}
